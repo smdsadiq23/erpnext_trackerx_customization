b40d201610e90b6da53f0cd825d8b8475e6eb8b1
Hassan Mudassir <hassan@cognitionx.tech>
2025-08-01 10:49:30 +0530

    Sales order changes , sales order item added order qty and tolerance percnetage

--- erpnext_trackerx_customization/public/js/sales_order.js
+++ erpnext_trackerx_customization/public/js/sales_order.js
@@ -1,3 +1,27 @@
 frappe.ui.form.on('Sales Order', {
+
   
 });
+
+
+frappe.ui.form.on('Sales Order Item', {
+  custom_order_qty: function (frm, cdt, cdn) {
+    update_qty_based_on_custom_fields(cdt, cdn);
+  },
+
+  custom_tolerance_percentage: function (frm, cdt, cdn) {
+    update_qty_based_on_custom_fields(cdt, cdn);
+  }
+});
+
+function update_qty_based_on_custom_fields(cdt, cdn) {
+  const row = locals[cdt][cdn];
+
+  const custom_qty = flt(row.custom_order_qty);
+  const tolerance_pct = flt(row.custom_tolerance_percentage);
+
+  if (custom_qty >= 0 && tolerance_pct >= 0) {
+    const qty = custom_qty + (custom_qty * tolerance_pct / 100);
+    frappe.model.set_value(cdt, cdn, 'qty', qty);
+  }
+}
