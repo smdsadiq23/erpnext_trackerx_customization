975314619f97243492c675e18ed369977f692012
Hassan Mudassir <hassan@cognitionx.tech>
2025-08-01 19:06:44 +0530

    work inprogress of work order changes

--- erpnext_trackerx_customization/public/js/work_order.js
+++ erpnext_trackerx_customization/public/js/work_order.js
@@ -1,167 +1,130 @@
-frappe.ui.form.on('Work Order', {
-    onload: function(frm) {
+// frappe.ui.form.on('Work Order', {
+//     onload(frm) {
+//         console.log('onload');
+//         frm.sales_order_item_map = {};
+//         frm.set_df_property('custom_work_order_line_items', 'cannot_add_rows', true);
+//         frm.set_df_property('custom_work_order_line_items', 'cannot_delete_rows', true);
+//     },
 
-        frm.sales_order_before_production_item = null;
-        frm.production_item_before = null;
-
-        if (!frm.doc.sales_order) {
-            frm.set_query('production_item', () => {
-                return {
-                    filters: [
-                        ['Item', 'name', '=', '__none__']
-                    ]
-                };
-            });
-        }
-
-        frm.fields_dict.production_item.$wrapper
-            .find('input[data-fieldname="production_item"]')
-            .on('click', function() {
-                frm.sales_order_before_production_item = frm.doc.sales_order;
-                if (!frm.doc.sales_order) {
-                    frappe.msgprint(__('Please select a Sales Order first.'));
-                }
-            });
-
-        frm.set_df_property('custom_work_order_line_items', 'cannot_add_rows', true);
-        frm.set_df_property('custom_work_order_line_items', 'cannot_delete_rows', true);
-    },
-
-    refresh: function(frm) {
-        if (!frm.doc.sales_order) {
-            //frm.set_value('production_item', null);
-        }
-        frm.is_user_selecting_production_item = false;
-
-        update_line_item_allocations(frm);
-    },
-
-    work_order_allocated_qty: function(frm) {
-        update_total_allocated_qty(frm);
-    },
-
-    sales_order: function(frm) {
-        frm.prev_sales_order = frm.doc.sales_order;
-
-        if (!frm.doc.sales_order) {
-            frm.set_query("production_item", () => ({
-                filters: [
-                    ["Item", "name", "=", "__none__"]
-                ],
-            }));
-            return;
-        }
-
-        // Step 1: Fetch Sales Order
-        frappe.call({
-            method: "frappe.client.get",
-            args: {
-                doctype: "Sales Order",
-                name: frm.doc.sales_order,
-            },
-            callback: function(r) {
-                if (!r.message) return;
-
-                const sales_order_data = r.message;
-                const item_codes = sales_order_data.items.map(row => row.item_code);
-
-                // Step 2: Set allowed production items based on SO
-                frm.set_query("production_item", () => ({
-                    filters: [
-                        ["Item", "item_code", "in", item_codes]
-                    ],
-                }));
-
-                // Step 3: Populate custom_work_order_line_items
-                frm.clear_table("custom_work_order_line_items");
-
-                (sales_order_data.items || []).forEach(item => {
-                    const child = frm.add_child("custom_work_order_line_items");
-                    child.line_item_no = item.custom_lineitem;
-                    child.size = item.custom_size;
-                    child.qty = item.qty;
-                    child.pending_qty = item.custom_pending_qty_for_work_order;
-                    child.already_allocated_qty = item.custom_allocated_qty_for_work_order;
-                });
-
-                frm.refresh_field("custom_work_order_line_items");
-            },
-        });
-    },
+//     production_item(frm) {
+//         // Set filter on sales orders
+//         console.log('production_item');
+//         if (frm.doc.production_item) {
+//             frappe.after_ajax(() => {
+//                 console.log('after_ajax');
+//                 console.log(frm.fields_dict.custom_sales_orders);
+//                 console.log(frm.fields_dict.custom_sales_orders.grid);
+//                 console.log(frm.fields_dict.custom_sales_orders.grid.get_field("sales_order"));
+//                 if (frm.fields_dict.custom_sales_orders && frm.fields_dict.custom_sales_orders.grid) {
+//                     frm.fields_dict.custom_sales_orders.grid.get_field("sales_order").get_query = () => ({
+//                         query: "frappe.desk.search.search_link",
+//                         filters: {
+//                             doctype: "Sales Order",
+//                             item_code: frm.doc.production_item
+//                         }
+//                     });
+//                 }
+//             });
+//         }
+//     },
 
 
-    custom_work_order_line_items_on_form_rendered: function(frm) {
-        // Update again when child table is rendered
-        update_line_item_allocations(frm);
-    },
+//     custom_sales_orders_on_form_rendered(frm) {
+//         console.log('custom_sales_orders_on_form_rendered');
+//         setup_sales_order_sync(frm);
+//     }
+// });
 
-    production_item: function(frm) {
-        if (!frm.doc.sales_order && frm.is_user_selecting_production_item) {
-            frappe.msgprint(__('Please select a Sales Order first.'));
-            //frm.set_value('production_item', null);
-        }
+// frappe.ui.form.on('Work Order Sales Orders', {
+//     sales_order: function (frm) {
+//         console.log('sales_order');
+//         setup_sales_order_sync(frm);
+//     }
+// });
 
-        if (frm.doc.production_item) {
-            frm.production_item_before = frm.doc.production_item;
-        }
+// function setup_sales_order_sync(frm) {
+//     console.log('setup_sales_order_sync');
+//     const selected_sos = (frm.doc.custom_sales_orders || []).map(row => row.sales_order);
 
-        //frm.is_user_selecting_production_item = false;
+//     // Load all Sales Orders' item data
+//     frappe.call({
+//         method: "frappe.client.get_list",
+//         args: {
+//             doctype: "Sales Order",
+//             filters: {
+//                 name: ["in", selected_sos]
+//             },
+//             fields: ["name"],
+//             limit_page_length: 1000
+//         },
+//         callback: function (res) {
+//             if (!res.message) return;
 
+//             // Get all SO Items
+//             frappe.call({
+//                 method: "frappe.client.get_list",
+//                 args: {
+//                     doctype: "Sales Order Item",
+//                     filters: {
+//                         parent: ["in", selected_sos],
+//                         item_code: frm.doc.production_item
+//                     },
+//                     fields: [
+//                         "name", "parent", "item_code", "custom_lineitem", "custom_size",
+//                         "qty", "custom_allocated_qty_for_work_order",
+//                         "custom_pending_qty_for_work_order"
+//                     ],
+//                     limit_page_length: 1000
+//                 },
+//                 callback: function (res2) {
+//                     const so_items = res2.message || [];
+//                     const existing_so_item_names = frm.doc.custom_work_order_line_items.map(i => i.sales_order_item);
 
-        setTimeout(() => {
-            if (!frm.doc.sales_order && frm.sales_order_before_production_item) {
+//                     const new_items = [];
 
-                frm.set_value('sales_order', frm.sales_order_before_production_item);
+//                     for (const item of so_items) {
+//                         if (existing_so_item_names.includes(item.name)) continue;
 
-            }
-            frm.sales_order_before_production_item = null;
-        }, 300); //
-    }
-});
+//                         const child = frm.add_child("custom_work_order_line_items");
+//                         child.sales_order_item = item.name;
+//                         child.line_item_no = item.custom_lineitem;
+//                         child.size = item.custom_size;
+//                         child.qty = item.qty;
+//                         child.already_allocated_qty = item.custom_allocated_qty_for_work_order;
+//                         child.pending_qty = item.custom_pending_qty_for_work_order;
+//                         child.work_order_allocated_qty = 1.0;
 
-frappe.ui.form.on('Work Order', {
-    production_item_on_form_rendered: function(frm) {
-        frm.is_user_selecting_production_item = false;
-    }
-});
+//                         new_items.push(child);
+//                     }
 
-frappe.ui.form.on('Work Order', {
-    production_item_focus: function(frm) {
-        frm.is_user_selecting_production_item = true;
-    }
-});
+//                     frm.refresh_field("custom_work_order_line_items");
+//                 }
+//             });
+//         }
+//     });
+// }
 
-function update_total_allocated_qty(frm) {
-    let total = 0;
-    (frm.doc.custom_work_order_line_items || []).forEach(row => {
-        total += flt(row.work_order_allocated_qty || 0);
-    });
-    frm.set_value('qty', total);
-}
+// // Remove Work Order Line Items if Sales Order deselected
+// frappe.ui.form.on('Work Order Sales Orders', {
+//     custom_sales_orders_remove: function (frm) {
+//         console.log('custom_sales_orders_remove');
+//         const selected_so_names = (frm.doc.custom_sales_orders || []).map(row => row.sales_order);
 
+//         const to_remove = frm.doc.custom_work_order_line_items.filter(row =>
+//             !selected_so_names.includes(get_sales_order_from_item(row.sales_order_item))
+//         );
 
-function update_line_item_allocations(frm) {
-    if (!frm.doc.sales_order) return;
+//         to_remove.forEach(row => {
+//             const idx = frm.doc.custom_work_order_line_items.findIndex(r => r.name === row.name);
+//             if (idx !== -1) frm.doc.custom_work_order_line_items.splice(idx, 1);
+//         });
 
-    frappe.call({
-        method: "frappe.client.get",
-        args: {
-            doctype: "Sales Order",
-            name: frm.doc.sales_order
-        },
-        callback: function(r) {
-            const sales_order_items = r.message.items || [];
+//         frm.refresh_field("custom_work_order_line_items");
+//     }
+// });
 
-            frm.doc.custom_work_order_line_items.forEach(child => {
-                const matching_item = sales_order_items.find(item =>
-                    item.custom_lineitem === child.line_item_no &&
-                    item.custom_size === child.size
-                );
-                if (matching_item) {
-                    frappe.model.set_value(child.doctype, child.name, 'already_allocated_qty', matching_item.custom_allocated_qty_for_work_order);
-                    frappe.model.set_value(child.doctype, child.name, 'pending_qty', matching_item.custom_pending_qty_for_work_order);
-                }
-            });
-        }
-    });
-}
+// function get_sales_order_from_item(sales_order_item) {
+//     console.log('get_sales_order_from_item');
+//     // sales_order_item is in format SO-0001-1, so parent is SO-0001
+//     return sales_order_item?.split("-").slice(0, -1).join("-");
+// }
