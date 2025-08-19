8da41aadc3cea842a279ae86e6f1c8589efc7a83
Hassan Mudassir <hassan@cognitionx.tech>
2025-08-19 15:33:15 +0530

    theme and logo changes

--- erpnext_trackerx_customization/hooks.py
+++ erpnext_trackerx_customization/hooks.py
@@ -85,9 +85,9 @@ after_migrate = [
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/erpnext_trackerx_customization/css/erpnext_trackerx_customization.css"
+app_include_css = "/assets/erpnext_trackerx_customization/css/erpnext_trackerx_customization.css"
 app_include_js = [
-    "/assets/erpnext_trackerx_customization/js/fabric_inspection_routes.js"
+    "/assets/erpnext_trackerx_customization/js/fabric_inspection_routes.js",
 ]
 
 # Removed Vue.js applications - using traditional interface
