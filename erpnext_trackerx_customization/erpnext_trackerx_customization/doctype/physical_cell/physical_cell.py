be1b03a205b17fe7901e48ea3d25dc773aadfc36
Hassan Mudassir <hassan@cognitionx.tech>
2025-09-02 18:51:16 +0530

    Physical Cell added validations on cell timings

--- erpnext_trackerx_customization/erpnext_trackerx_customization/doctype/physical_cell/physical_cell.py
+++ erpnext_trackerx_customization/erpnext_trackerx_customization/doctype/physical_cell/physical_cell.py
@@ -7,6 +7,13 @@ from frappe.model.document import Document
 
 class PhysicalCell(Document):
 	def validate(self):
+		self.validate_for_operations_from_supported_operation_group()
+		self.validate_for_physical_cell_timings()
+		self.validate_for_cell_break_timings()
+
+
+
+	def validate_for_operations_from_supported_operation_group(self):
 		"""Validate that selected operations belong to the correct operation group"""
 		if not self.supported_operation_group:
 			return
@@ -20,6 +27,38 @@ class PhysicalCell(Document):
 					frappe.throw(
 						f"Operation '{row.operation}' in row {row.idx} does not belong to the selected Operation Group '{self.supported_operation_group}'"
 					)
+
+	
+	def  validate_for_physical_cell_timings(self):
+		if not self.start_time or not self.end_time:
+			frappe.throw(
+				f"Cell timing is mandatory, Please provide valid start and end time"
+			)
+		
+		if self.start_time >= self.end_time:
+			frappe.throw(
+				f"Cell Start time should be before end time"
+			)
+
+	def validate_for_cell_break_timings(self):
+
+		if self.cell_breaks:
+			for cell_break in self.cell_breaks:
+				if cell_break.break_start >= cell_break.break_end:
+					frappe.throw(
+						f"Invalid Break Time, Start time should be before the end time at row: {cell_break.idx}"
+					)
+
+				if cell_break.break_start < self.start_time:
+					frappe.throw(
+						f"Invalid break time at row: {cell_break.idx}, Break time should be within the cell timings, Cannot start before cell timing"
+					)
+				
+				if cell_break.break_end > self.end_time:
+					frappe.throw(
+						f"Invalid break time at row: {cell_break.idx}, Break time should be within the cell timings, Cannot end after cell timing"
+					)
+				
 
 
 
