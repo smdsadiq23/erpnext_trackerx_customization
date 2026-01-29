1b76048dff8ad3708a305692e9a90c859caadf6d
Vishal94-tech <vishal@cognitionx.tech>
2026-01-30 00:17:32 +0530

    New Basic Doctypes created

---
+++ erpnext_trackerx_customization/erpnext_trackerx_customization/doctype/yarn_content_master/yarn_content_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, CognitionX and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Yarn Content Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
